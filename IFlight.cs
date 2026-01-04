1e31ea4783d7bd40d3e627896193bedf48f66de8
Madeiane <diana.chitimia27@gmail.com>
2026-01-04 14:55:56 +0200

    add core interfaces for flight and ticketing system

---
+++ IFlight.cs
@@ -0,0 +1,19 @@
+﻿namespace Flight_Reservations;
+using System;
+using System.Collections.Generic;
+
+//initial interface for a flight
+public interface IFlight
+{
+    string FlightNumber { get; set; }
+    DateTime DepartureTime { get; set; }
+    DateTime ArrivalTime { get; set; }
+    string DepartureAirportId { get; set; }
+    string ArrivalAirportId { get; set; }
+    string AircraftId { get; set; }
+    int TotalSeats { get; }
+    int AvailableSeats { get; }
+    decimal BasePrice { get; set; }
+
+    bool IsFullyBooked(); //checks if the flight is fully booked
+}
